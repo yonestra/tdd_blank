968fd0212a54101e851720d14e575ac74e6db6e1
Shota Yonezawa <s.yone101@gmail.com>
2012-03-17 15:21:34 +0900

    use Parameterized

--- test/tdd/WikiEngineTest.java
+++ test/tdd/WikiEngineTest.java
@@ -3,44 +3,44 @@ package tdd;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class WikiEngineTest {
-
 	public WikiEngine wikiEngine = new WikiEngine();
+
+	private String param;
 	
-	@Test
-	public void h1_Headline() throws Exception {
-		String actual = wikiEngine.format("= Headline =");
-		assertThat(actual, is("<h1>Headline</h1>"));
+	private String expected;
+	
+	@Parameters
+	public static List<String[]> getParameters(){
+		String[][] params = {
+				{ "= Headline =", "<h1>Headline</h1>" },
+				{ "= Footline =", "<h1>Footline</h1>" }, 
+				{ null, "" },
+				{ "", "" },
+				{ "== Headline ==", "<h2>Headline</h2>" },
+		};
+		return Arrays.asList(params);
+	}
+
+	public WikiEngineTest(String param, String expected) {
+		this.param = param;
+		this.expected = expected;
 	}
 	
 	@Test
-	public void h1_Footline() throws Exception {
-		String actual = wikiEngine.format("= Footline =");
-		assertThat(actual, is("<h1>Footline</h1>"));
-	}
-	
-	@Test
-	public void h1_Null() throws Exception {
-		String actual = wikiEngine.format(null);
-		assertThat(actual, is(""));
-	}
-	
-	@Test
-	public void h1_blank() throws Exception {
-		String actual = wikiEngine.format("");
-		assertThat(actual, is(""));
-	}
-	
-	@Test
-	public void h2_Headline() throws Exception {
-		String actual = wikiEngine.format("== Headline ==");
-		assertThat(actual, is("<h2>Headline</h2>"));
+	public void format() throws Exception {
+		String actual = wikiEngine.format(param);
+		assertThat(actual, is(expected));
 	}
 }
