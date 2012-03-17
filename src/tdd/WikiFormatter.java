1fd7e39bcef0ab63aa0636430f193dad06f5c65e
Shota Yonezawa <s.yone101@gmail.com>
2012-03-17 14:34:06 +0900

    first test h1

--- src/tdd/WikiFormatter.java
+++ src/tdd/WikiFormatter.java
@@ -2,4 +2,9 @@ package tdd;
 
 public class WikiFormatter {
 
+	public String h1(String string) {
+		
+		return "<h1>Headline</h1>";
+	}
+
 }
--- test/tdd/WikiFormatterTest.java
+++ test/tdd/WikiFormatterTest.java
@@ -1,12 +1,21 @@
 package tdd;
 
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class WikiFormatterTest {
 
-
+	@Test
+	public void h1() throws Exception {
+		WikiFormatter formatter = new WikiFormatter();
+		String actual = formatter.h1("= Headline =");
+		assertThat(actual, is("<h1>Headline</h1>"));
+	}
 }
